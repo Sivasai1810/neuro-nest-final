af8f2bda8e8b7e4c26491fd176f7dde5fb39266c
NALLAMALLI SWAMY SIVA SAI <nsivasai88@gmail.com>
2025-08-06 00:06:52 +0530

    Update live.jsx

--- client/src/features/live/live.jsx
+++ client/src/features/live/live.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import io from "socket.io-client"
-const socket=io("http://localhost:3000")
-const Chat=()=>{
-const [userName,setUserName]=useState("")
-const [message,setMessage]=useState("")
-const [chat,setChat]=useState([])
+// import { useEffect } from "react"
+// import { useState } from "react"
+// import io from "socket.io-client"
+// const socket=io("http://localhost:3000")
+// const Chat=()=>{
+// const [userName,setUserName]=useState("")
+// const [message,setMessage]=useState("")
+// const [chat,setChat]=useState([])
 
-return(
-    <div>
+// return(
+//     <div>
         
-    </div>
-)
+//     </div>
+// )
 
 
-}
-export default Chat
+// }
+// export default Chat
